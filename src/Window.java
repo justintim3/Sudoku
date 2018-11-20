abb0650a74c5bca9fc442b3b182765a8c871189c
Justin Lin <justlin90@gmail.com>
2018-11-20 01:23:22 -0800

    Changed WINDOW_SIZE variable to BOARD_SIZE for clarity.

--- src/Window.java
+++ src/Window.java
@@ -3,7 +3,7 @@ import java.awt.Insets;
 
 public class Window {
 	public Window(Display puzzleDisplay) {
-		final int WINDOW_SIZE = Display.TILE_SIZE * puzzleDisplay.getPuzzle().getN();
+		final int BOARD_SIZE = Display.TILE_SIZE * puzzleDisplay.getPuzzle().getN();
 		JFrame frame = new JFrame("Sudoku");
 		
 		frame.setResizable(false);
@@ -11,8 +11,8 @@ public class Window {
 		frame.add(puzzleDisplay);
 		frame.setVisible(true);
 		Insets insets = frame.getInsets();
-		frame.setSize(WINDOW_SIZE + insets.left + insets.right, 
-			WINDOW_SIZE + insets.top + insets.bottom);
+		frame.setSize(BOARD_SIZE + insets.left + insets.right, 
+			BOARD_SIZE + insets.top + insets.bottom);
 		frame.setLocationRelativeTo(null);
 	}
 }
